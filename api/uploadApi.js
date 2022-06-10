29dbf657f38948dc7b7902696f1e717014d585e6
GautamGambhir <ggcapital8@gmail.com>
2022-06-11 01:06:38 +0530

    changes

--- api/uploadApi.js
+++
@@ -1,8 +0,0 @@
-import requestMaker from "./networkHandler/requestMaker"
-
-export const documentsUpload = (payload) =>
-{
-    const url = 'upload/';
-    const params = {};
-    return requestMaker(url,"post",{params,payload});
-}
