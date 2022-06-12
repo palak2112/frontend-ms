c1a05c5cb9c96d906f51a2a602b688a5b6c676fb
AARAMBH KESHRI <meaarambh@gmail.com>
2022-06-13 03:45:18 +0530

    feat: profile page integration

--- User/horizon-ui-chakra-main/src/views/admin/profile/components/General.js
+++ User/horizon-ui-chakra-main/src/views/admin/profile/components/General.js
@@ -2,11 +2,14 @@
 import { SimpleGrid, Text, useColorModeValue } from "@chakra-ui/react";
 // Custom components
 import Card from "components/card/Card.js";
-import React from "react";
+import React,{useState,useEffect} from "react";
 import Information from "views/admin/profile/components/Information";
 import Banner from "views/admin/profile/components/Banner";
 import banner from "assets/img/auth/banner.png";
 import avatar from "assets/img/avatars/avatar4.png";
+
+import { fetchUserById } from "../../../../api/userApi";
+
 // Assets
 export default function GeneralInformation(props) {
   const { ...rest } = props;
@@ -17,6 +20,25 @@ export default function GeneralInformation(props) {
     "0px 18px 40px rgba(112, 144, 176, 0.12)",
     "unset"
   );
+
+  const [userData,setUserData] = useState();
+    const getUserDetails = async () =>{
+    const uid = JSON.parse(localStorage.getItem("user"))["id"];
+      try {
+          const res = await fetchUserById(uid);
+          console.log(res.data.data);
+          setUserData(res.data.data);
+
+      } catch (error) {
+        
+      }
+    }
+
+    useEffect(() => {
+      getUserDetails();
+    }, [])
+    
+
   return (
     <Card mb={{ base: "0px", "2xl": "20px" }} {...rest}>
       {/* <Text
@@ -30,8 +52,8 @@ export default function GeneralInformation(props) {
       <Banner
           gridArea='2 / 2 / 2 / 2'
           banner={banner}
-          avatar={avatar}
-          name='User Name'
+          avatar={userData && userData['photoUrl']}
+          name={userData && userData['name']}
           // job='Product Designer'
           // posts='17'
           // followers='9.7k'
@@ -44,31 +66,36 @@ export default function GeneralInformation(props) {
         <Information
           boxShadow={cardShadow}
           title='UID'
-          value='show-uid'
+          value={userData && userData['uid']}
         />
         <Information
           boxShadow={cardShadow}
           title='Email'
-          value='user@gmail.com'
+          value={userData && userData['email']}
         />
        
         
         <Information
           boxShadow={cardShadow}
           title='Year of Enrollment'
-          value='2015'
+          value={userData && userData['yearOfEnrolment']}
         />
         
         <Information
           boxShadow={cardShadow}
           title='Phone No'
-          value='9999999999'
+          value={userData && userData['phoneNumber']}
         />
         
         <Information
           boxShadow={cardShadow}
           title='Address'
-          value='line 1'
+          value= {userData?.address &&
+            Object.keys(userData?.address)
+              .map(function (key) {
+                return userData.address[key];
+              })
+              .join(", ")}
         />
       </SimpleGrid>
     </Card>
